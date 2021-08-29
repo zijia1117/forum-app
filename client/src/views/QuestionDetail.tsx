2ee21fa9417aa366967fcbae5d6359de0b04bd9f
zijia <lizijia1117@163.com>
2021-08-29 23:27:28 +0100

    Update QuestionDetail.tsx
    回答问题

--- client/src/views/QuestionDetail.tsx
+++ client/src/views/QuestionDetail.tsx
@@ -1,15 +1,20 @@
-import { useEffect, useState } from "react";
-import { Divider, message } from "antd";
+import { createRef, useEffect, useState } from "react";
+import { Input, Divider, message, Button } from "antd";
+
 import { useParams } from "react-router-dom";
 import { AnswerItem } from "./components/AnswerItem";
 import "./../styles/QuestionDetail.scss";
 import { QuestionDetail as QuestionDetailModel } from "../models/QuestionModel";
+import { TextAreaRef } from "antd/lib/input/TextArea";
+
+const { TextArea } = Input;
 
 export function QuestionDetail(props: any) {
 	const { id: postId } = useParams<{ id: string }>();
 
 	const [question, setQuestion] = useState<QuestionDetailModel>();
 
+	const answerTextArea = createRef<TextAreaRef>();
 	useEffect(() => {
 		if (question) {
 			if (postId === ":id") {
@@ -24,6 +29,37 @@ export function QuestionDetail(props: any) {
 			});
 		});
 	});
+
+	function postAnswer() {
+		const answer =
+			answerTextArea.current?.resizableTextArea?.textArea.textContent;
+		if (!answer || answer === "") {
+			message.warn("please type your answer");
+			return;
+		}
+
+		const data = {
+			username: JSON.parse(sessionStorage.getItem("user")!).username,
+			content: answer,
+		};
+
+		fetch("http://localhost:5000/questions/answer/" + postId, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application/json",
+			},
+			body: JSON.stringify({
+				...data,
+			}),
+			mode: "cors",
+			cache: "default",
+		}).then((res) => {
+			res.json().then((json) => {
+				setQuestion(json);
+			});
+		});
+	}
 
 	return (
 		<div className="post-detail">
@@ -59,6 +95,21 @@ export function QuestionDetail(props: any) {
 			{question?.answers?.map((answer, index) => {
 				return <AnswerItem key={index} answer={answer}></AnswerItem>;
 			})}
+
+			<Divider
+				orientation="left"
+				style={{ fontWeight: "bold", fontSize: "24px" }}>
+				{">"}Post your answer{"<"}
+			</Divider>
+			<TextArea
+				autoSize={{ minRows: 5 }}
+				showCount
+				maxLength={500}
+				ref={answerTextArea}></TextArea>
+			<br />
+			<Button type="primary" onClick={postAnswer}>
+				Post your Answer
+			</Button>
 		</div>
 	);
 }
