ee8cf89bd9c1fb07f8f891ed0c86aabe472a52d4
zijia <lizijia1117@163.com>
2021-08-29 22:38:02 +0100

    Update QuestionModel.ts
    types

--- client/src/models/QuestionModel.ts
+++ client/src/models/QuestionModel.ts
@@ -6,8 +6,32 @@ export interface Question {
 	content: string;
 	time: Date;
 	username: string;
-	comments: ObjectId[];
-	answers?: ObjectId[];
+	comments?: ObjectId[] | Comment[];
+	answers?: ObjectId[] | Answer[];
+}
+
+export interface QuestionDetail {
+	_id?: ObjectId;
+	title: string;
+	content: string;
+	time: Date;
+	username: string;
+	comments?: Comment[];
+	answers?: Answer[];
+}
+export interface Answer {
+	_id?: ObjectId;
+	username: string;
+	content: string;
+	comments?: ObjectId[];
+	time?: Date;
+}
+
+interface Comment {
+	_id?: ObjectId;
+	username: string;
+	content: string;
+	time?: Date;
 }
 
 export {};
